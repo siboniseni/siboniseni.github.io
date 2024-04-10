64ae1d8e4f2f41ba567b4f7745db0a4676043f6e
siboniseni <siboniseni.kasa@outlook.com>
2024-04-10 14:20:34 +0200

    UI outlook modified

---
+++ script.js
@@ -0,0 +1,30 @@
+var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+   
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw7ULD1tWvTQoYDOjOOikFjfTuNw5OZfzovVAcx3SHDS7n7bS1QvPUUoENGIiuaFg_0eg/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },4000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
